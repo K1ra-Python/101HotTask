69e1f0e1ca6116dc43b309decfd70f13cd56d183
K1ra-Python <makismka.guchenko@mail.ru>
2024-02-05 05:15:06 +0300

    #

--- app/Views/comments/index.php
+++ app/Views/comments/index.php
@@ -6,6 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.css') ?>" type="text/css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
@@ -39,7 +40,7 @@
     <a href="<?= base_url('/sort_by_date_down') ?>" class="btn btn-primary">Сортировать по Дате (по убыванию)</a>
 </div>
 
-    <div class="d-flex flex-row mb-3 justify-content-around mt-5 border border-3">
+    <div class="d-flex flex-row mb-3 justify-content-around mt-5 border border-3 flex-wrap">
         <!-- Список комментариев -->
         <?php foreach ($comments as $comment): ?>
 
